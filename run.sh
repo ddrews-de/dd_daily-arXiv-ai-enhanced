699bbb3949c92c5843306afd87df0cea46f42f70
Daniel Drews <github@ddrews.de>
2025-10-13 11:51:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ today=`date -u "+%Y-%m-%d"`
 cd daily_arxiv
 scrapy crawl arxiv -o ../data/${today}.jsonl
 
-cd ../ai
-python enhance.py --data ../data/${today}.jsonl
+# cd ../ai
+# python enhance.py --data ../data/${today}.jsonl
 
-cd ../to_md
-python convert.py --data ../data/${today}_AI_enhanced_${LANGUAGE}.jsonl
+# cd ../to_md
+# python convert.py --data ../data/${today}_AI_enhanced_${LANGUAGE}.jsonl
 
-cd ..
-python update_readme.py
+# cd ..
+# python update_readme.py
